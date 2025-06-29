8b6ebf4d7def9750bd86a8394591b7640b0d9af7
serhatgithub <stekin010@gmail.com>
2025-06-29 05:19:34 +0300

    Last version : All of Fixed

--- task-ui/src/LoginPage.jsx
+++ task-ui/src/LoginPage.jsx
@@ -66,7 +66,7 @@ function LoginPage({ onLogin }) {
       <div className="inner-box">
         <div className="header">
           <h2 style={{ color: "#691bf1" }}>
-            {isRegistering ? "Kayıt Ol" : "GİRİŞ YAP - Yeni Versiyon"}
+            {isRegistering ? "Kayıt Ol" : "Giriş Yap"}
           </h2>
           <button
             className="register-btn"
